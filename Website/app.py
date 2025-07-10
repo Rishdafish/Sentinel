a66e67cc64c80f0042e68c7f5f28688ff7e53569
Rishi B <rishabh.biry@gmail.com>
2025-07-09 20:37:33 -0400

    register/login Page done

--- Website/app.py
+++ Website/app.py
@@ -1,41 +1,41 @@
 import asyncio 
+import json
 from flask import Flask, render_template, request, session, redirect, url_for
+from google.cloud import storage
+import re
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 
-PIN_CODE = "1213461346234623642323462310"
+def searchforEmail(userEmail : str):
+    client = storage.Client()
+    bucket = client.bucket('data_for_website')
+    key = f"users/{userEmail.lower().strip()}.json"
+    blob = bucket.blob(key)
+    if not blob.exists(): 
+        print('User Email is not Found')
+        return "Email Does not Exist"
+    else: 
+        return blob
+
+def verifyCreds():
+    pass
+
+def register():
+    pass
 
 @app.route('/')
 def home():
     return render_template("MainPage.html")
 
-@app.route('/access', methods=['GET', 'POST'])
-def GetAccessPage():
-    if request.method == 'POST':
-        pin = request.form.get('pin')
-        if pin == PIN_CODE:
-            session['role'] = 'admin'
-            return redirect(url_for('GetProjectPage'))
-        elif 'guest' in request.form:
-            session['role'] = 'guest'
-            return redirect(url_for('GetProjectPage'))
-        else:
-            return render_template('Access.html', error="Invalid PIN.")
-    if session.get('role') in ['admin', 'guest']:
-        return redirect(url_for('GetProjectPage'))
+@app.route('/register', methods=['POST', 'GET'])
+def registerUser():
+    pass
 
+@app.route('/registerPageTypeShit')
+def registerPage():
     return render_template('Access.html')
 
-@app.route('/projects')
-def GetProjectPage():
-    role = session.get('role')
-    if role in ['admin', 'guest']:
-        if role == 'admin':
-            editable = True
-        return render_template('Projects.html', editable=editable)
-    return redirect(url_for('GetAccessPage'))
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
